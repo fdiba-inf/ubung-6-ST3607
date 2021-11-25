3f742dc4e88c370d4557fa0af7063a446df089a7
ST3607 <>
2021-11-25 16:56:17 +0000

    Files

--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -5,5 +5,35 @@ import java.util.Scanner;
 public class PasswordCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        boolean passValid = false;
+        do {
+          System.out.println("Enter password: ");
+          String password = input.nextLine();
+          if (password.length() < 8){
+            continue;
+          }
+
+          boolean allLetersAndDigits = true;
+          int digitCount = 0;
+          for (int index = 0; index < password.length(); index++){
+          char symbol = password.charAt(index);
+          boolean letterOrDigit = Character.isLetterOrDigit(symbol);
+          if(!letterOrDigit) {
+            allLetersAndDigits = false;
+            break;
+          }
+          boolean digtit = Character.isDigit(symbol);
+          if (digtit) {
+            digitCount++;
+          }
+          }
+
+          if (allLetersAndDigits && digitCount >=2){
+            passValid = true;
+          }
+        } while (!passValid);
+        System.out.println("Password valid! ");
+
     }
 }
